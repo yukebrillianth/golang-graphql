1752d217255e86ce52321854fad345e39f1dd6c6
yuke brilliant <yukebrilliant@gmail.com>
2021-08-19 08:13:09 +0700

    Query and Link Mutation

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -11,12 +11,37 @@ import (
 	"github.com/yukebrillianth/golang-graphql/go-gql/graph/model"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
+	var link model.Link
+	var user model.User
+	link.Address = input.Address
+	link.Title = input.Title
+	user.Name = "test"
+	link.User = &user
+	return &link, nil
+}
+
+func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) (string, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+func (r *mutationResolver) Login(ctx context.Context, input model.Login) (string, error) {
 	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) RefreshToken(ctx context.Context, input model.RefreshTokenInput) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
+	var links []*model.Link
+	dummyLink := model.Link {
+		Title: "Our dummy link",
+		Address: "https://address.org",
+		User: &model.User{Name: "admin"},
+	}
+		links = append(links, &dummyLink)
+		return links, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
@@ -27,3 +52,16 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+// func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewLink) (*model.Todo, error) {
+// 	panic(fmt.Errorf("not implemented"))
+// }
+// func (r *queryResolver) Todos(ctx context.Context) ([]*model.Link, error) {
+// 	panic(fmt.Errorf("not implemented"))
+// }
